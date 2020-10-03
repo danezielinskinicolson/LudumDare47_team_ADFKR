f0f3093910232cec8a11b73b353a1337f4728b3c
danezielinskinicolson <danezielinskinicolson@gmail.com>
2020-10-03 15:16:04 +1000

    Die grass

--- GodotProject/world/grass.gd
+++ GodotProject/world/grass.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://world/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
 		
-func _ready():
-	pass # Replace with function body.
+func create_grass_effect():
+	var GrassEffect = load("res://world/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+	queue_free()
 
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
