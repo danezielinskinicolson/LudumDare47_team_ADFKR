2f4015da069fbafe94af97b24665d0a45ac6d40f
danezielinskinicolson <danezielinskinicolson@gmail.com>
2020-10-03 14:00:33 +1000

    basic states working
    attack state operational

--- GodotProject/Player/Player.gd
+++ GodotProject/Player/Player.gd
@@ -5,12 +5,31 @@ const MAX_SPEED = 120
 const ACC = 500
 const FRIC = 900
 
+enum{
+	MOVE,
+	ATTACK,
+	INVEN,
+	INTERACT
+}
+
+var state = MOVE
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		INVEN:
+			pass
+		INTERACT:
+			pass
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,6 +38,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/run/blend_position", input_vector)
+		animationTree.set("parameters/attack/blend_position", input_vector)
 		animationState.travel("run")
 		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACC*delta)
 		
@@ -26,7 +46,23 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRIC*delta)
 		animationState.travel("idle")
 		
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity)	
 	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+func attack_state(delta):
+	print("attack state")
+	animationState.travel("attack")
+	pass
+	
+func inventory_state(delta):
+	pass
+	
+func interact_state(delta):
+	pass	
+
+func attack_animation_finish():
+	state = MOVE
+
 func _ready():
-	pass # Replace with function body.
+	animationTree.active = true
